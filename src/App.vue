224c231e5f4ed1a293bb3c71074b94989cdd14a2
xwzkj <xwzkjbili@qq.com>
2024-06-07 16:46:20 +0800

    ..?

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ import { useUserStore } from "@/stores/user";
 import { usePlayStore } from '@/stores/play'
 import MessageApi from "@/modules/messageApi.vue";
 import Container from "./pages/container.vue";
+import emitter from "@/utils/mitt";
+let themeOverrides = ref({});
 let userStore = useUserStore();
 let playStore = usePlayStore();
 userStore.updateByStorage();
-
-
 onMounted(() => {
   //更新用户信息
   if (userStore.isLogin === true && Date.now() - userStore.updateTime > 1000 * 60 * 3) {//三分钟
@@ -19,16 +19,22 @@ onMounted(() => {
     playStore.playlistInit()
   }
 })
+emitter.on('changeTheme', (theme) => {
+  themeOverrides.value = { ...themeOverrides.value, ...theme }
+  console.log(theme,themeOverrides);
+})
 
 
 </script>
 
 <template>
   <div>
-    <Container />
-    <n-message-provider>
-      <messageApi />
-    </n-message-provider>
+    <n-config-provider :theme-overrides="themeOverrides">
+      <Container />
+      <n-message-provider>
+        <messageApi />
+      </n-message-provider>
+    </n-config-provider>
   </div>
 </template>
 
--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -1,16 +1,35 @@
 <template>
     <div>
-        <n-button @click="logout">退出登录</n-button>
-        <n-button @click="loginByCookie">输入cookie登录</n-button>
-        <n-button @click="tishi">弹出一个message</n-button>
-        <n-button @click="update">马上更新用户信息</n-button>
-        <n-button @click="showCk">显示cookie</n-button>
+        <div class="setting">
+            <div class="button">
+                <n-button @click="logout">退出登录</n-button>
+            </div>
+            <div class="button">
+                <n-button @click="loginByCookie">输入cookie登录</n-button>
+            </div>
+            <div class="button">
+                <n-button @click="tishi">弹出一个message</n-button>
+            </div>
+            <div class="button">
+                <n-button @click="update">马上更新用户信息</n-button>
+            </div>
+            <div class="button">
+                <n-button @click="showCk">显示cookie</n-button>
+            </div>
+            <div class="color">
+                <span>选择primary颜色</span>
+                <n-color-picker class="color-picker" v-model:value="primaryColor" :show-alpha="false" :modes="['hex']" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup name="setting" lang="js">
 import { useUserStore } from '@/stores/user'
 import * as api from '@/modules/api'
+import { generate } from '@ant-design/colors'
+import emitter from '@/utils/mitt';
+let primaryColor = ref('')
 let userStore = useUserStore()
 function logout() {
     userStore.logout();
@@ -32,6 +51,45 @@ function showCk() {
     let ck = document.cookie + ' ' + userStore.cookie
     alert(ck)
 }
+watch(primaryColor, (value) => {
+    let colors = generate(value)
+    console.log(value);
+    emitter.emit('changeTheme', {
+        common: {
+            primaryColor: colors[5],
+            primaryColorHover: colors[4],
+            primaryColorSuppl: colors[4],
+            primaryColorPressed: colors[6]
+        }
+    })
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+
+.setting {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.button {
+    margin: 1rem;
+    width: fit-content;
+}
+
+.color {
+    margin: 1rem;
+    display: flex;
+    align-items: center;
+}
+
+.color-picker {
+    width: 8rem;
+    height: 2rem;
+}
+</style>
