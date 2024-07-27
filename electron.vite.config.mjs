7085cfca094fa7be93e275d96c47252c6c2ffbf0
xwzkj <xwzkjbili@qq.com>
2024-07-27 12:23:31 +0800

    编译问题

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -72,19 +72,23 @@ export default defineConfig({
       assetsDir: 'assets',
       chunkSizeWarningLimit: 2000, // 解决包大小超过500kb的警告
       rollupOptions: {
-        output: {
-          manualChunks: {},
-          chunkFileNames: 'assets/[name]-[hash].js',
-          entryFileNames: 'assets/[name]-[hash].js',
-          assetFileNames: 'assets/[name]-[hash].[ext]',
-          // 解决文件名中的非法字符
-          sanitizeFileName: (name) => {
-            const match = DRIVE_LETTER_REGEX.exec(name)
-            const driveLetter = match ? match[0] : ''
-            return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          input: {
+            index: resolve(__dirname, 'src/renderer/index.html'),
+            lyric: resolve(__dirname, 'src/renderer/desktopLyric.html')
+          },
+          output: {
+            manualChunks: {},
+            chunkFileNames: 'assets/[name]-[hash].js',
+            entryFileNames: 'assets/[name]-[hash].js',
+            assetFileNames: 'assets/[name]-[hash].[ext]',
+            // 解决文件名中的非法字符
+            sanitizeFileName: (name) => {
+              const match = DRIVE_LETTER_REGEX.exec(name)
+              const driveLetter = match ? match[0] : ''
+              return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            }
           }
         }
       }
     }
-  }
 })
